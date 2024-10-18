80c7ad4343f422d7aef02f69a0b55e4936ff0644
decrinoa <decrinoa@gmail.com>
2024-10-18 18:34:33 +0200

    new test for chemical_potential and my_laplacian

--- testing.py
+++ testing.py
@@ -8,14 +8,34 @@ Created on Wed Oct  16 16:45:43 2024
 """
 
 import Cahn_Hilliard
-import configuration
+import configparser
 import numpy as np
 from hypothesis import strategies as st
 from hypothesis import given
 
-#test that all the values of c are between the low and up bounds 
-@given( Nx = st.integers(1,configuration.Nx), Ny = st.integers(1,configuration.Ny))
+
+config = configparser.ConfigParser()
+config.read('configuration.ini')
+
+Nx = config['settings']['Nx']
+Ny = config['settings']['Ny']
+dx = config['settings']['dx']
+dy = config['settings']['dy']
+
+c0 = config['settings']['c0']
+dc = config['settings']['dc']
+
+Nx = int(Nx)
+Ny = int(Ny)
+dx = float(dx)
+dy = float(dy)
+
+c0 = float(c0)
+dc = float(dc)
+ 
+@given( Nx = st.integers(1,Nx), Ny = st.integers(1,Ny), c0 = st.floats(0, c0), dc = st.floats(0, dc))
 def test_add_fluctuation_boundaries(Nx,Ny,c0,dc):
+    #Test that all the values of c are between the low and up bounds
     c = Cahn_Hilliard.add_fluctuation(Nx, Ny, c0, dc)
     lower_bound = c0 - dc*(0.5)
     upper_bound = c0 + dc*(0.5)
@@ -24,3 +44,17 @@ def test_add_fluctuation_boundaries(Nx,Ny,c0,dc):
     assert np.all(c <= upper_bound)
 
 
+@given(Nx = st.integers(1,Nx), Ny = st.integers(1,Ny))
+def test_chemical_potential_shape(Nx, Ny):
+    # Test output shape matches input shape with specific values
+    c = np.random.rand(Nx, Ny)
+    A = 1
+    result = Cahn_Hilliard.chemical_potential(c, A)
+    assert result.shape == c.shape
+
+@given(Nx = st.integers(1,Nx), Ny = st.integers(1,Ny), dx = st.floats(0.1,dx), dy = st.floats(0.1,dy))
+def test_my_laplacian_shape(Nx, Ny, dx, dy):
+    # Test output shape matches input shape with specific values
+    c = np.random.rand(Nx, Ny)
+    result = Cahn_Hilliard.my_laplacian(c, dx, dy)
+    assert result.shape == c.shape
