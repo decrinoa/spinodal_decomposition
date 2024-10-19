6d660bde25f1ddbda52de23fc12fc7331aa5b662
decrinoa <decrinoa@gmail.com>
2024-10-19 19:31:14 +0200

    new test function with documentation

--- testing.py
+++ testing.py
@@ -63,6 +63,34 @@ def test_add_fluctuation_boundaries(Nx,Ny,c0,dc):
     assert np.all(c >= lower_bound)
     assert np.all(c <= upper_bound)
 
+@given(c = st.floats(0, 1))
+def test_chemical_potential_boundaries(c):
+    """
+    This test ensures that the chemical potential values calculated by the 
+    'chemical_potential' function are within specified bounds. The bounds are defined
+    from the expression of 'chemical_potential', where the maxima condition is 'c = 0.5' 
+    and the minima condition is 'c = 0'.
+
+    Parameters:
+    ----------   
+    c : The concentration value generated between 0 and 1.
+       
+    Assertions:
+    -----------
+    - Asserts that all chemical potential values are greater than or equal to 'chemical_potential(0, A)'.
+    - Asserts that all chemical potential values are less than or equal to 'chemical_potential(0.5, A)'.
+    """
+    c_max = 0.5 - 1 / (2*np.sqrt(3))
+    c_min = 0.5 + 1 / (2*np.sqrt(3))
+    A = 1.0
+    lower_bound = Cahn_Hilliard.chemical_potential(c_min, A)
+    upper_bound = Cahn_Hilliard.chemical_potential(c_max, A)
+    result = Cahn_Hilliard.chemical_potential(c, A)
+    
+    assert np.all(result >= lower_bound)
+    assert np.all(result <= upper_bound)
+
+
 @given(Nx = st.integers(1,Nx), Ny = st.integers(1,Ny))
 def test_chemical_potential_shape(Nx, Ny):
     """
@@ -110,33 +138,4 @@ def test_my_laplacian_shape(Nx, Ny, dx, dy):
     c = np.random.rand(Nx, Ny)
     result = Cahn_Hilliard.my_laplacian(c, dx, dy)
     
-    assert result.shape == c.shape
-    
-@given(Nx = st.integers(1,Nx), Ny = st.integers(1,Ny), dx = st.floats(0.1,dx), dy = st.floats(0.1,dy))
-def test_my_laplacian_boundaries(Nx, Ny, dx, dy):
-    """
-    This test verifies that the output of the 'my_laplacian' function 
-    has the same shape as the input concentration matrix 'c'.
-
-    Parameters:
-    ----------
-    Nx : The number of columns in the concentration matrix. Must be greater or equal than 1 and lower or euqual than Nx in configuration file.
-       
-    Ny : The number of rows in the concentration matrix. Must be greater or equal than 1 and lower or euqual than Ny in configuration file.
-
-    dx : The spacing between points in the x-direction. Must be greater or equal than 0.1 and lower or equal than dx in configuration file.
-    
-    dy : The spacing between points in the y-direction. Must be greater or equal than 0.1 and lower or equal than dy in configuration file.
-    
-    Assertions:
-    -----------
-    - Asserts that the first row of the output from 'my_laplacian' matches the last row.
-    
-    - Asserts that the first column of the output from 'my_laplacian' matches the last column.
-    """
-    np.random.seed(24)
-    c = np.random.rand(Nx, Ny)
-    result = Cahn_Hilliard.my_laplacian(c, dx, dy)
-    
-    assert np.allclose(result[0, :], result[-1, :])
-    assert np.allclose(result[:, 0], result[:, -1]) 
+    assert result.shape == c.shape    
