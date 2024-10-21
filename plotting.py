bd145c52c27c25244439b8bd8682414f4dcc274c
decrinoa <decrinoa@gmail.com>
2024-10-21 11:04:14 +0200

    Modified plotting.py so that we save 10 frames of the simulation, addition of this ten frame in images folder

--- plotting.py
+++ plotting.py
@@ -10,12 +10,15 @@ import configparser
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 config = configparser.ConfigParser()
 config.read('configuration.ini')
 
 Nx = config['settings']['Nx']
 Ny = config['settings']['Ny']
+
+
 
 Nx = int(Nx)
 Ny = int(Ny)
@@ -37,7 +40,10 @@ def load_results_from_csv(filename='simulation_results.csv'):
     
     return results
 
-def plot_results(results):
+def plot_results(results, folder='images'):
+    
+    os.makedirs(folder, exist_ok=True)
+    
     plt.figure(figsize=(12, 6))
 
     for time, c, mu_c in results:
@@ -51,10 +57,15 @@ def plot_results(results):
         plt.colorbar(im_m, label='μ (x,y)', aspect=40)
         plt.title(f'Chemical potential at time {time:.2f} s')
     
-
         plt.tight_layout()
+        
+        if time % 50 == 0:
+            plt.savefig(os.path.join(folder, f'plot_at_time_{time:.2f}.png'))
+            print(f'Saved plot for time {time:.2f} s')
+            
         plt.pause(0.1)
 
     plt.show()
+    
 
 plot_results(load_results_from_csv(filename='simulation_results.csv'))
